7357fa7088c2a5ac83e113351bed1d85ef61bf82
Joe Clark <joclark@caci.com>
2018-07-12 14:17:35 -0600

    name check for geopackage list

--- mapcache/src/main/java/mil/nga/mapcache/GeoPackageManagerFragment.java
+++ mapcache/src/main/java/mil/nga/mapcache/GeoPackageManagerFragment.java
@@ -4261,10 +4261,10 @@ public class GeoPackageManagerFragment extends Fragment implements
 
         public void onBindViewHolder(GeoPackageViewHolder holder, int position) {
             //Use the provided View Holder on the onCreateViewHolder method to populate the current row on the RecyclerView
-            if (list.get(position).isEmpty()) {
+            if (!databases.isEmpty()) {
                 holder.title.setText(databases.get(position));
             } else {
-                holder.title.setText(list.get(position).get(0).getDatabase());
+                holder.title.setText("Not found");
             }
             // Get the count of tile tables and feature tables associated with each geopackage list to set counts
             int tileTables = 0;

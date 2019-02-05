7e38483c40a798065b8ed7eee51e38b3a373833e
Joe Clark <jclark118@gmail.com>
2019-02-05 10:41:31 -0700

    added tutorial activity and fragment

---
+++ mapcache/src/main/java/mil/nga/mapcache/tutorial/TutorialActivity.java
@@ -0,0 +1,37 @@
+package mil.nga.mapcache.tutorial;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+import mil.nga.mapcache.R;
+
+public class TutorialActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preferences);
+        getSupportActionBar().setTitle("Tutorial");
+        // Adds back arrow button to action bar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportFragmentManager().beginTransaction().replace(R.id.content_frame, new TutorialFragment()).commit();
+    }
+
+    /**
+     * Add back arrow button listener
+     * @param item
+     * @return
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+}

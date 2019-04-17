1d2065132d067de093beb41edcbbf77c7fcb3c55
Joe Clark <jclark118@gmail.com>
2019-04-17 15:20:25 -0600

    change click listener to pass back a GeoPackageDatabase instead of the just the Database name

---
+++ mapcache/src/main/java/mil/nga/mapcache/view/GeoPackageClickListener.java
@@ -0,0 +1,18 @@
+package mil.nga.mapcache.view;
+
+import android.view.View;
+
+import mil.nga.mapcache.data.GeoPackageDatabase;
+
+/**
+ * Listener for clicking on a GeoPackage object in the main RecyclerView
+ */
+public interface GeoPackageClickListener {
+    /**
+     * Implement on click
+     * @param view View clicked
+     * @param position position in the RecyclerView
+     * @param db GeoPackageDatabase object of the row that was clicked on
+     */
+    void onClick(View view, int position, GeoPackageDatabase db);
+}

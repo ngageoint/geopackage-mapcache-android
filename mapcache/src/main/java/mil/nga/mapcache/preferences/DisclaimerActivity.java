3173da16d28b11e28b9bec100c39a8cecad3c208
Joe Clark <joclark@caci.com>
2019-06-12 16:00:10 -0600

    add disclaimer to the settings menu

---
+++ mapcache/src/main/java/mil/nga/mapcache/preferences/DisclaimerActivity.java
@@ -0,0 +1,36 @@
+package mil.nga.mapcache.preferences;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.AppCompatActivity;
+import mil.nga.mapcache.R;
+
+public class DisclaimerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preferences);
+        getSupportActionBar().setTitle("Disclaimer");
+        // Adds back arrow button to action bar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportFragmentManager().beginTransaction().replace(R.id.content_frame, new DisclaimerFragment()).commit();
+    }
+
+    /**
+     * Add back arrow button listener
+     * @param item
+     * @return
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
---
+++ mapcache/src/main/java/mil/nga/mapcache/preferences/DisclaimerFragment.java
@@ -0,0 +1,28 @@
+package mil.nga.mapcache.preferences;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.preference.PreferenceFragmentCompat;
+import mil.nga.mapcache.R;
+
+public class DisclaimerFragment extends PreferenceFragmentCompat {
+
+    View disclaimerView;
+
+    public DisclaimerFragment() {
+    }
+
+    @Override
+    public void onCreatePreferences(Bundle bundle, String s) {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        disclaimerView = inflater.inflate(R.layout.fragment_disclaimer, container, false);
+        return disclaimerView;
+    }
+}

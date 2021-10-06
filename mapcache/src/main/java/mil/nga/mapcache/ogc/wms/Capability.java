806c841f9325405c95aa64689a0262133f2e8e33
Brandon Cleveland <bcleveland1022@gmail.com>
2021-10-06 10:15:41 -0600

    The capability object within a getCapabilities xml

---
+++ mapcache/src/main/java/mil/nga/mapcache/ogc/wms/Capability.java
@@ -0,0 +1,23 @@
+package mil.nga.mapcache.ogc.wms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * The capability object within a WMS getCapabilities xml
+ */
+public class Capability {
+
+    /**
+     * The layers for the wms service.
+     */
+    private List<Layer> layer = new ArrayList<>();
+
+    /**
+     * Gets the layers for the wms service.
+     * @return The layers for the wms service with child layers.
+     */
+    public List<Layer> getLayer() {
+        return layer;
+    }
+}

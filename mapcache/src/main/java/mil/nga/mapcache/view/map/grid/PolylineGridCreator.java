902f08ee871315ad91da862c406d6c2d3aeabc2a
Brandon Cleveland <bcleveland1022@gmail.com>
2021-11-24 11:01:38 -0700

    Creates google map PolylineOptions based on the grids within the model

---
+++ mapcache/src/main/java/mil/nga/mapcache/view/map/grid/PolylineGridCreator.java
@@ -0,0 +1,56 @@
+package mil.nga.mapcache.view.map.grid;
+
+import android.graphics.Color;
+
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import mil.nga.geopackage.BoundingBox;
+
+/**
+ * Creates the polylines that can be added to the map.
+ */
+public class PolylineGridCreator {
+
+    /**
+     * The grid model to update.
+     */
+    private GridModel gridModel;
+
+    /**
+     * Constructor.
+     *
+     * @param gridModel The grid model to update.
+     */
+    public PolylineGridCreator(GridModel gridModel) {
+        this.gridModel = gridModel;
+    }
+
+    /**
+     * Creates the polylines based on the grids within the model.
+     */
+    public void createPolylines() {
+        List<PolylineOptions> polylines = new ArrayList<>();
+
+        for (Grid grid : gridModel.getGrids()) {
+            BoundingBox box = grid.getBounds();
+            LatLng lowerLeft = new LatLng(box.getMinLatitude(), box.getMinLongitude());
+            LatLng lowerRight = new LatLng(box.getMinLatitude(), box.getMaxLongitude());
+            LatLng upperRight = new LatLng(box.getMaxLatitude(), box.getMaxLongitude());
+            LatLng upperLeft = new LatLng(box.getMaxLatitude(), box.getMinLongitude());
+
+            PolylineOptions polyline = new PolylineOptions();
+            polyline.add(lowerLeft, lowerRight, upperRight, upperLeft, lowerLeft);
+            polyline.width(5);
+            polyline.color(Color.RED);
+            polyline.geodesic(true);
+            polylines.add(polyline);
+        }
+
+        PolylineOptions[] newLines = polylines.toArray(new PolylineOptions[0]);
+        this.gridModel.setPolylines(newLines);
+    }
+}

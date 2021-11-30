2e83fcb27f493ad1bcf0b904d1043a2c42698a38
Brandon Cleveland <bcleveland1022@gmail.com>
2021-11-30 14:16:25 -0700

    reduce polylines sent to map

--- mapcache/src/main/java/mil/nga/mapcache/view/map/grid/PolylineGridCreator.java
+++ mapcache/src/main/java/mil/nga/mapcache/view/map/grid/PolylineGridCreator.java
@@ -6,8 +6,13 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.PolylineOptions;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
+import kotlin.random.jdk8.PlatformThreadLocalRandom;
 import mil.nga.geopackage.BoundingBox;
 
 /**
@@ -34,6 +39,8 @@ public class PolylineGridCreator {
      */
     public void createPolylines() {
         List<PolylineOptions> polylines = new ArrayList<>();
+        //Reduce the number of polylines on map
+        Map<LatLng, Set<LatLng>> existingPolylines = new HashMap<>();
 
         for (Grid grid : gridModel.getGrids()) {
             BoundingBox box = grid.getBounds();
@@ -43,11 +50,65 @@ public class PolylineGridCreator {
             LatLng upperLeft = new LatLng(box.getMaxLatitude(), box.getMinLongitude());
 
             PolylineOptions polyline = new PolylineOptions();
-            polyline.add(lowerLeft, lowerRight, upperRight, upperLeft, lowerLeft);
-            polyline.width(5);
-            polyline.color(Color.BLACK);
-            polyline.geodesic(false);
-            polylines.add(polyline);
+
+            if (!existingPolylines.containsKey(lowerLeft) || !existingPolylines.get(lowerLeft).contains(lowerRight)) {
+                polyline.add(lowerLeft, lowerRight);
+                polyline.width(5);
+                polyline.color(Color.BLACK);
+                polyline.geodesic(false);
+                polylines.add(polyline);
+
+                if (!existingPolylines.containsKey(lowerLeft)) {
+                    existingPolylines.put(lowerLeft, new HashSet<>());
+                }
+
+                existingPolylines.get(lowerLeft).add(lowerRight);
+            }
+
+            if (!existingPolylines.containsKey(lowerRight) || !existingPolylines.get(lowerRight).contains(upperRight)) {
+                polyline = new PolylineOptions();
+                polyline.add(lowerRight, upperRight);
+                polyline.width(5);
+                polyline.color(Color.BLACK);
+                polyline.geodesic(false);
+                polylines.add(polyline);
+
+                if (!existingPolylines.containsKey(lowerRight)) {
+                    existingPolylines.put(lowerRight, new HashSet<>());
+                }
+
+                existingPolylines.get(lowerRight).add(lowerRight);
+            }
+
+            if (!existingPolylines.containsKey(upperLeft) || !existingPolylines.get(upperLeft).contains(upperRight)) {
+                polyline = new PolylineOptions();
+                polyline.add(upperRight, upperLeft);
+                polyline.width(5);
+                polyline.color(Color.BLACK);
+                polyline.geodesic(false);
+                polylines.add(polyline);
+
+                if (!existingPolylines.containsKey(upperLeft)) {
+                    existingPolylines.put(upperLeft, new HashSet<>());
+                }
+
+                existingPolylines.get(upperLeft).add(upperRight);
+            }
+
+            if (!existingPolylines.containsKey(lowerLeft) || !existingPolylines.get(lowerLeft).contains(upperLeft)) {
+                polyline = new PolylineOptions();
+                polyline.add(upperLeft, lowerLeft);
+                polyline.width(5);
+                polyline.color(Color.BLACK);
+                polyline.geodesic(false);
+                polylines.add(polyline);
+
+                if (!existingPolylines.containsKey(lowerLeft)) {
+                    existingPolylines.put(lowerLeft, new HashSet<>());
+                }
+
+                existingPolylines.get(lowerLeft).add(upperLeft);
+            }
         }
 
         PolylineOptions[] newLines = polylines.toArray(new PolylineOptions[0]);

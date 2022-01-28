c1e02707dd15d3dadb1697baa86ed56b48b82803
Brandon Cleveland <bcleveland1022@gmail.com>
2022-01-28 12:30:21 -0700

    checking to see if volley works

--- mapcache/src/main/java/mil/nga/mapcache/io/network/HttpClient.java
+++ mapcache/src/main/java/mil/nga/mapcache/io/network/HttpClient.java
@@ -1,6 +1,21 @@
 package mil.nga.mapcache.io.network;
 
 import android.app.Activity;
+import android.util.Base64;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
 
 import mil.nga.mapcache.utils.ThreadUtils;
 
@@ -13,6 +28,8 @@ public class HttpClient {
      * The instance of this class.
      */
     private static final HttpClient instance = new HttpClient();
+
+    private RequestQueue queue;
 
     /**
      * Gets the instance of this class.
@@ -31,8 +48,15 @@ public class HttpClient {
      * @param activity Used to get the app name and version for the user agent.
      */
     public void sendGet(String url, IResponseHandler handler, Activity activity) {
-        HttpGetRequest request = new HttpGetRequest(url, handler, activity);
-        ThreadUtils.getInstance().runBackground(request);
+        //HttpGetRequest request = new HttpGetRequest(url, handler, activity);
+        //hreadUtils.getInstance().runBackground(request);
+        if(queue == null) {
+            queue = Volley.newRequestQueue(activity);
+        }
+
+        // Request a string response from the provided URL.
+        VolleyGetRequest request = new VolleyGetRequest(url, handler);
+        queue.add(request);
     }
 
     /**

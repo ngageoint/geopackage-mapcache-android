48f02442ab73d42cbebc90e2b6da3a22aa8f9301
Brandon Cleveland <bcleveland1022@gmail.com>
2022-07-08 16:12:28 -0600

    Remove unused manager fragment

--- mapcache/src/main/java/mil/nga/mapcache/MainActivity.java
+++ mapcache/src/main/java/mil/nga/mapcache/MainActivity.java
@@ -66,13 +66,6 @@ public class MainActivity extends AppCompatActivity {
      */
     private GeoPackageMapFragment mapFragment;
 
-    /**
-     * Manager fragment
-     */
-//    private GeoPackageManagerFragment managerFragment;
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -211,7 +204,7 @@ public class MainActivity extends AppCompatActivity {
      */
     @Override
     public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
-
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         // Check if permission was granted
         boolean granted = grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED;
 

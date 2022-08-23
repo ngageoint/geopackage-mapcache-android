71fee6568fa97680c2ead58d72e3beb131faacb8
Brandon Cleveland <bcleveland1022@gmail.com>
2022-08-23 14:55:01 -0600

    Class that pulls out the xml from the html

--- mapcache/src/main/java/mil/nga/mapcache/io/network/WebViewXmlExtractor.java
+++ mapcache/src/main/java/mil/nga/mapcache/io/network/WebViewXmlExtractor.java
@@ -1,11 +1,22 @@
 package mil.nga.mapcache.io.network;
 
+import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
+/**
+ * Extracts the xml from within the web page.
+ */
 public class WebViewXmlExtractor implements WebViewExtractor {
 
     @Override
     public InputStream extractContent(String html) {
-        return null;
+        int newLineIndex = html.indexOf("\\n\\n") + 4;
+        String xml = html.substring(newLineIndex, html.length() - 1);
+        xml = xml.replace("\\u003C", "<");
+        xml = xml.replace("\\\"", "\"");
+        xml = xml.replace("\\n", "");
+
+        return new ByteArrayInputStream(xml.getBytes(StandardCharsets.UTF_8));
     }
 }

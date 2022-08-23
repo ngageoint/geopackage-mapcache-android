e5e9726440e3a57f8238476f42aa89ec24c627a1
Brandon Cleveland <bcleveland1022@gmail.com>
2022-08-23 12:45:04 -0600

    Add comments

--- mapcache/src/main/java/mil/nga/mapcache/io/network/WebViewExtractor.java
+++ mapcache/src/main/java/mil/nga/mapcache/io/network/WebViewExtractor.java
@@ -2,7 +2,17 @@ package mil.nga.mapcache.io.network;
 
 import java.io.InputStream;
 
+/**
+ * Given a web pages html, the extractor will retrieve certain content from the web page to be used
+ * within an InputStream.
+ */
 public interface WebViewExtractor {
 
+    /**
+     * Extracts certain contents from the given html.
+     *
+     * @param html The html to extract data from.
+     * @return The extracted content.
+     */
     InputStream extractContent(String html);
 }

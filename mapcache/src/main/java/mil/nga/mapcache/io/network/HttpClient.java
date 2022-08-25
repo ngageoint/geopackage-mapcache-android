3f2e4d6625a93686f914238ad3b3481bb521506c
Brandon Cleveland <bcleveland1022@gmail.com>
2022-08-25 15:13:42 -0600

    Bypass HttpRequest if host is known for webpage logins

--- mapcache/src/main/java/mil/nga/mapcache/io/network/HttpClient.java
+++ mapcache/src/main/java/mil/nga/mapcache/io/network/HttpClient.java
@@ -1,8 +1,14 @@
 package mil.nga.mapcache.io.network;
 
 import android.app.Activity;
+import android.util.Log;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 import mil.nga.mapcache.utils.ThreadUtils;
 
@@ -22,6 +28,11 @@ public class HttpClient implements SessionManager {
     private final Map<String, Map<String, String>> allCookies = new HashMap<>();
 
     /**
+     * The hosts that require a web view in order to download from given urls.
+     */
+    private final Set<String> webViewHosts = new HashSet<>();
+
+    /**
      * Gets the instance of this class.
      *
      * @return This class instance.
@@ -38,8 +49,21 @@ public class HttpClient implements SessionManager {
      * @param activity Used to get the app name and version for the user agent.
      */
     public void sendGet(String url, IResponseHandler handler, Activity activity) {
-        HttpGetRequest request = new HttpGetRequest(url, handler, this, activity);
-        ThreadUtils.getInstance().runBackground(request);
+        boolean requiresWebView = false;
+
+        try {
+            URL theUrl = new URL(url);
+            requiresWebView = webViewHosts.contains(theUrl.getHost());
+        } catch (MalformedURLException e) {
+            Log.e(HttpClient.class.getSimpleName(), e.getMessage(), e);
+        }
+
+        if(requiresWebView) {
+            requestRequiresWebView(url, handler, activity);
+        } else {
+            HttpGetRequest request = new HttpGetRequest(url, handler, this, activity);
+            ThreadUtils.getInstance().runBackground(request);
+        }
     }
 
     /**
@@ -59,7 +83,13 @@ public class HttpClient implements SessionManager {
     }
 
     @Override
-    public void requestRequiresWebView(String url, IResponseHandler handler, Activity activity) {
+    public synchronized void requestRequiresWebView(String url, IResponseHandler handler, Activity activity) {
+        try {
+            URL theUrl = new URL(url);
+            webViewHosts.add(theUrl.getHost());
+        } catch (MalformedURLException e) {
+            Log.e(HttpClient.class.getSimpleName(), e.getMessage(), e);
+        }
         activity.runOnUiThread(()->{
             WebViewRequest request = new WebViewRequest(url, handler, activity);
             request.execute();

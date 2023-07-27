8337f4225f0acc3a0a16e5679d5c2410e257a15c
Joe Clark <jclark118@gmail.com>
2023-07-27 13:50:26 -0600

    update PreferenceManager to use androidx version for getDefaultSharedPreferences

--- mapcache/src/main/java/mil/nga/mapcache/GeoPackageMapFragment.java
+++ mapcache/src/main/java/mil/nga/mapcache/GeoPackageMapFragment.java
@@ -19,7 +19,7 @@ import android.os.Bundle;
 import android.os.Looper;
 import android.os.VibrationEffect;
 import android.os.Vibrator;
-import android.preference.PreferenceManager;
+import androidx.preference.PreferenceManager;
 import android.text.Editable;
 import android.text.InputType;
 import android.text.TextWatcher;

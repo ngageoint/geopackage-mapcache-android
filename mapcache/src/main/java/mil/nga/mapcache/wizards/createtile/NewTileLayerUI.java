c4f8ebd64429d73b36d218cc17a9e06f2d586681
Joe Clark <jclark118@gmail.com>
2023-07-27 15:22:22 -0600

    preference manager library update

--- mapcache/src/main/java/mil/nga/mapcache/wizards/createtile/NewTileLayerUI.java
+++ mapcache/src/main/java/mil/nga/mapcache/wizards/createtile/NewTileLayerUI.java
@@ -4,7 +4,7 @@ import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
+import androidx.preference.PreferenceManager;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;

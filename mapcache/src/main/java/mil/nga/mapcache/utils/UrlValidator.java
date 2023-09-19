279573c92e32bd47f0fa49c87f342bad33caaab6
Joe Clark <jclark118@gmail.com>
2023-09-19 14:05:56 -0600

    add validation to confirm tile url is either xyz or wms

--- mapcache/src/main/java/mil/nga/mapcache/utils/UrlValidator.java
+++ mapcache/src/main/java/mil/nga/mapcache/utils/UrlValidator.java
@@ -11,27 +11,40 @@ import mil.nga.mapcache.R;
 public class UrlValidator {
 
     /**
-     * Determine if the url is valid based on whether it has xyz or not
-     * @param context
-     * @param url
-     * @return true if the url has xyz info
+     * A URL is valid if it has either xyz or wms params
+     * @param context - context for grabbing string resources
+     * @param url url to validate
+     * @return true if the url has xyz or wms info
      */
     public static boolean isValidTileUrl(Context context, String url){
-        return hasXYZ(context, url);
+        if(hasXYZ(context, url) || hasWms(url)){
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Determine if the url is valid based on whether it has proper wms or not
+     * @param url url to validate
+     * @return true if the url has xyz info
+     */
+    public static boolean hasWms(String url){
+        if (url.toLowerCase().contains("wms")) {
+            return true;
+        }
+        return false;
     }
 
 
     /**
      * Determine if the url has x, y, or z variables
      *
-     * @param url
+     * @param url url to look for xyz data
      * @return true if it's a valid xyz url
      */
     public static boolean hasXYZ(Context context, String url) {
-
         String replacedUrl = replaceXYZ(context, url, 0, 0, 0);
         boolean hasXYZ = !replacedUrl.equals(url);
-
         return hasXYZ;
     }
 
@@ -39,14 +52,13 @@ public class UrlValidator {
     /**
      * Replace x, y, and z in the url
      *
-     * @param url
-     * @param z
-     * @param x
-     * @param y
-     * @return
+     * @param url url to modify
+     * @param z integer to replace the Z param with
+     * @param x integer to replace the X param with
+     * @param y integer to replace the Y param with
+     * @return string with the xyz portion replaced with the given int values
      */
     private static String replaceXYZ(Context context, String url, int z, long x, long y) {
-
         url = url.replaceAll(
                 context.getResources().getString(R.string.tile_generator_variable_z),
                 String.valueOf(z));
